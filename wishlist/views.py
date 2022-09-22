3cece62f8bbb9ca3c78f3d14c5d1ed93a9d5b28b
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-09-22 13:08:56 +0700

    Lab 3 PBP

--- wishlist/views.py
+++ wishlist/views.py
@@ -14,14 +14,13 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
-    return render(request, "wishlist.html", context)
-
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Felix Tjahyadi',
     'last_login': request.COOKIES['last_login'],
 }
+    return render(request, "wishlist.html", context)
 
 def xml(request):
     data = BarangWishlist.objects.all()
